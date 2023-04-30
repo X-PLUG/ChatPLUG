440cb846b75dc967241c36bc30df138c0b949762
rgtjf <tjf141457@alibaba-inc.com>
2023-04-30 19:34:34 +0800

    use readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,26 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
 
 project = 'ChatPLUG'
 copyright = '2017-2023, XPLUG Team'
 author = 'XPLUG Team'
-
 extensions = [
+    "sphinx.ext.intersphinx",
     "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.githubpages"
 ]
+
 myst_enable_extensions = [
     "colon_fence",
 ]
+myst_url_schemes = ["http", "https", ]
+
+source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- docs/index.md
+++ docs/index.md
@@ -1,16 +1,24 @@
-
 Welcome to ChatPLUG's documentation!
 ====================================
 
-```{toctree}
-:maxdepth: 1
-:caption: "Contents:"
-   
-chatplug/get_started
 
-get_started
-nlu_training
-develop
-faq
-about_us
+```{eval-rst}
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Get Started
+
+   chatplug/get_started
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents
+
+   get_started
+   nlu_training
+   develop
+   faq
+   chatplug/get_started
+   about_us
+
 ```
